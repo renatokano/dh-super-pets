2c06c2376722c32db06bcc7a3efab82534a62d61
renatokano <renatokano16@gmail.com>
2020-05-17 07:22:14 -0300

    Update: an ORM test running 2 inner-joins

--- controllers/dbtestController.js
+++ controllers/dbtestController.js
@@ -1,9 +1,17 @@
 const Sequelize = require('sequelize');
+// env ~> our environment (production or development)
 const env = process.env.NODE_ENV || 'development';
+// based on our environment, get the correct db configuration file
 const config = require(__dirname + '/../config/database.json')[env];
+// create a connection w/ the database
 const db = new Sequelize(config);
+// import some sequelize models
+const { Neighborhood, City, State } = require('../models/index');
 
+
+// define our controller
 const controller = {
+  // just a db authentication test
   dbconnection: async (req, res) => {
     try {
       await db.authenticate();
@@ -12,6 +20,7 @@ const controller = {
       res.send('Unable to connect to the database:', error);
     }
   },
+  // a raw-query example
   select_all: async (req, res) => {
     const {entity} = req.params;
     try {
@@ -23,6 +32,7 @@ const controller = {
       res.send(`Error:`, error);
     }
   },
+  // another raw-query example
   select_from_id: async (req, res) => {
     const {entity, id} = req.params;
     console.log(`SELECT * FROM ${entity} WHERE id=${id}`);
@@ -34,6 +44,23 @@ const controller = {
     } catch (error) {
       res.send(`Error:`, error);
     }
+  },
+  // an example using ORM (2 inner-joins)
+  select_all_neighborhoods: async (req, res) => {
+    const neighborhoods = await Neighborhood.findAll({
+      order: [
+        ['name', 'ASC']
+      ],
+      include: [{
+        model: City,
+        required: true,
+        include: {
+          model: State,
+          required: true
+        }
+      }]
+    });
+    return res.send(neighborhoods);
   }
 }
 
--- routes/dbtestRoutes.js
+++ routes/dbtestRoutes.js
@@ -2,14 +2,26 @@ const express = require('express');
 const dbtestController = require('../controllers/dbtestController');
 const route = express.Router();
 
-// test database connection
+/* 
+  Test Database Connection
+  e.g. http://localhost:3000/dbtest/
+*/
 route.get('/', dbtestController.dbconnection);
 
-// SELECT * FROM neighborhoods
+/*
+  ORM [neighborhoods <-> cities <-> states] (2 inner-joins)
+
+  SELECT `Neighborhood`.`id`, `Neighborhood`.`name`, `Neighborhood`.`city_id`, `City`.`id` AS `City.id`, `City`.`name` AS `City.name`, `City`.`state_id` AS `City.state_id`, `City->State`.`id` AS `City.State.id`, `City->State`.`name` AS `City.State.name` FROM `neighborhoods` AS `Neighborhood` INNER JOIN `cities` AS `City` ON `Neighborhood`.`city_id` = `City`.`id` INNER JOIN `states` AS `City->State` ON `City`.`state_id` = `City->State`.`id` ORDER BY `Neighborhood`.`name` ASC;
+
+  e.g. http://localhost:3000/dbtest/neighborhoods/cities 
+*/
+route.get('/neighborhoods/cities', dbtestController.select_all_neighborhoods);
+
+// SELECT * FROM neighborhoods (raw-queries)
 // e.g.: http://localhost:3000/dbtest/neighborhoods/all
 route.get('/:entity/all', dbtestController.select_all);
 
-// SELECT * FROM pet_types WHERE id=1
+// SELECT * FROM pet_types WHERE id=1 (raw-queries)
 // e.g.: http://localhost:3000/dbtest/pet_types/1
 route.get('/:entity/:id', dbtestController.select_from_id);
 
