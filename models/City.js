36efc7afe59d33a96ce5cc1fed5305f05d9483da
renatokano <renatokano16@gmail.com>
2020-05-17 07:20:22 -0300

    Add: City model

---
+++ models/City.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, Datatypes) => {
+  const City = sequelize.define (
+    "City",
+    {
+      id: {
+        type: Datatypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: Datatypes.STRING(150),
+        allowNull: false
+      },
+      state_id: {
+        type: Datatypes.INTEGER,
+        allowNull: false
+      }
+    },
+    {
+      timestamps: false,
+      tableName: 'cities'
+    }
+  );
+
+  // Relationships
+  City.associate = (models) => {
+    City.belongsTo(models.State, {
+      foreignKey: "state_id"
+    });
+    City.hasMany(models.Neighborhood, {
+      foreignKey: "id"
+    });
+  };
+
+  return City;
+}
