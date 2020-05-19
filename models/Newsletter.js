aceafa7e046a2d7624fa412be77eb28adf8eb8c8
EudesJS <eudes.silva2013@gmail.com>
2020-05-19 03:46:55 -0300

    Add: Newsletter model

---
+++ models/Newsletter.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, Datatypes) => {
+  const Newsletter = sequelize.define (
+    "Newsletter",
+    {
+      id: {
+        type: Datatypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      email: {
+        type: Datatypes.STRING(190),
+        allowNull: false,
+        unique:true
+      },
+      created_at: Datatypes.DATE
+    },
+    {
+      timestamps: false,
+      tableName: 'newsletter'
+    }
+  );
+
+  return Newsletter;
+}
+
