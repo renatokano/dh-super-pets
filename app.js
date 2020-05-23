464b92f1778837be2abf0ac152f3445ee38f9bb8
renatokano <renatokano16@gmail.com>
2020-05-23 08:20:13 -0300

    Add: Session configuration

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const methodOverride = require('method-override');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -14,9 +17,18 @@ const dbtestRoutes = require('./routes/dbtestRoutes');
 
 // Middlewares
 app.use(express.static(path.join(__dirname, 'public')));
+app.set("views", path.join(__dirname, "views"));
 app.set('view engine','ejs');
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended:true}));
+app.use(
+  session({
+    secret: "HJn2pWEiKbemzxLoEy4GfzqEvubdMPpn9RbvDYKHPtw1iybfNkmVYf7rYoozEU",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(cookieParser())
 
 // Routes
 app.use(indexRoutes);
