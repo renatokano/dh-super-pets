9a433fe7241aa361e96284d4b8f1cb4bd8874a6c
renatokano <renatokano16@gmail.com>
2020-05-24 23:27:50 -0300

    BugFix: locals.client <- key "client" is reserved (EJS)

--- middleware/clientAuthentication.js
+++ middleware/clientAuthentication.js
@@ -2,7 +2,7 @@ module.exports = (req, res, next) => {
   // client not logged in
   if(!req.session.client) res.redirect('/');
   // client has a session
-  res.locals.user = req.session.client; // <- don't use res.locals.client
+  res.locals.user_session = req.session.client; // <- don't use res.locals.client
   // In the later versions of EJS the "client" parameter key is reserved, 
   // and will therefore cause errors. j/ rename the key and resolve the issue. 
   next();
