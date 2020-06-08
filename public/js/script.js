c47bf96724dee69e84f56bbbde3d097b66f7eace
adria-menezes <adriah.menezes@gmail.com>
2020-06-07 21:04:49 -0300

    Update: css and js

--- public/js/script.js
+++ public/js/script.js
@@ -12,3 +12,18 @@ function openUserTabs(e, tabName){
   document.getElementById(tabName).style.display="grid";
   e.currentTarget.style.fontWeight = 'bold';
 }
+
+function openProfessionalsTabs(e, tabName){
+  var i, tabs, tabLinks;
+  tabs = document.getElementsByClassName("pap-tabs");
+  for(i=0; i<tabs.length; i++){
+    tabs[i].style.display = 'none';
+  }
+  tabLinks = document.getElementsByClassName("tabLinks");
+  for(i=0; i<tabs.length; i++){
+    tabLinks[i].style.fontWeight = 'normal';
+  }
+  console.log(`tabName: ${tabName}`);
+  document.getElementById(tabName).style.display="grid";
+  e.currentTarget.style.fontWeight = 'bold';
+}
