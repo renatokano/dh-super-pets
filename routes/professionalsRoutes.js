9d4bc1f1c16f403f0ecd00b50cf3d0c1ba1c6d37
adria-menezes <adriah.menezes@gmail.com>
2020-06-07 21:09:03 -0300

    Update: Professional Routes

--- routes/professionalsRoutes.js
+++ routes/professionalsRoutes.js
@@ -1,22 +1,38 @@
 const express = require('express');
 const professionalsController = require('../controllers/professionalsController');
+const coverageareasController = require('../controllers/coverageareasController');
+const authProfessionalController = require('../controllers/authProfessionalController');
+const professionalServicesController = require('../controllers/professionalServicesController');
+const route = express.Router();
+const professionalAuthentication = require('../middleware/professionalAuthentication');
+const upload = require('../middleware/professionalsUpload');
 
-let route = express.Router();
 
-// display a list of all professionals
-route.get('/', professionalsController.index);
+route.get('/login', authProfessionalController.professionalCreate);
+route.post('/login', authProfessionalController.professionalStore);
 
 // get a form for creating a new professional
 route.get('/new', professionalsController.new);
 
 // display an admin area 
-route.get('/:id/admin', professionalsController.admin);
+route.get('/:id/admin', professionalAuthentication, professionalsController.admin);
 
 // get a form for editing a professional
-route.get('/:id/edit', professionalsController.edit);
+route.put('/:id/edit', professionalAuthentication, upload.any(), professionalsController.put);
+
+// create new
+route.post('/:id/neighborhoods', professionalAuthentication, coverageareasController.create);
+route.get('/:id/:idP/neighborhoods', professionalAuthentication, coverageareasController.delete);
+
+
+route.post('/:id/services', professionalAuthentication, professionalServicesController.create);
+route.get('/:id/:idS/services', professionalAuthentication, professionalServicesController.delete);
 
 // display a specific professional
 route.get('/:id', professionalsController.show);
+
+// create a new user
+route.post('/create', professionalsController.create);
 
 // create a new professional
 // route.post('/', (req,res)=>{
