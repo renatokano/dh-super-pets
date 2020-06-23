711d4d1c898a95795650f710b942d385c85a14d7
renatokano <renatokano16@gmail.com>
2020-06-23 14:02:17 -0300

    Add: flash messages to 'Authentication' middleware #97

--- middleware/professionalAuthentication.js
+++ middleware/professionalAuthentication.js
@@ -1,6 +1,9 @@
 module.exports = (req, res, next) => {
   // professional not logged in
-  if(!req.session.professional) res.redirect('/home/index');
+  if(!req.session.professional) {
+    req.flash('error', 'Usuário deve estar logado para continuar! Faça o login e tente novamente.');
+    return res.redirect('/');
+  }
   // professional has a session
   res.locals.professional_session = req.session.professional;
   next();
