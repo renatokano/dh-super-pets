42af228402c81909ca57a7e56cc7a056e90edb40
renatokano <renatokano16@gmail.com>
2020-06-27 05:15:58 -0300

    Add: transactional email (for new professionals) #118

--- controllers/professionalsController.js
+++ controllers/professionalsController.js
@@ -7,6 +7,7 @@ const bcrypt = require('bcrypt');
 const { saltRounds } = require('../config/bcrypt');
 const moment = require('../config/moment');
 const cloudinary = require('../config/cloudinary');
+const sgMail = require('../config/sendgrid');
 
 const controller = {
   show: async (req,res)=>{
@@ -271,6 +272,7 @@ const controller = {
       }
 
       // success
+      generateEmail(professional, true); // send a welcome email to the professional
       req.flash('success', `Seja bem vindo(a) ${name}! Sua conta profissional foi criada com sucesso!`);
       return res.redirect(`/professionals/${uuid}/admin`); 
 
@@ -553,6 +555,49 @@ const getAllPetTypes = async function (){
       ]
     }
   );
+}
+
+const generateEmail = async function(professional, newProfessional=true){
+  let html = '';
+  let text = '';
+  let subject = '';
+
+  if(newProfessional){
+    html = `<strong>Seja bem vindo(a) ${professional.name} a comunidade SuperPets Profissional!</strong>
+      <br>
+      <p>É um prazer tê-lo conosco.</p>
+      <p>Para iniciar sua jornada, retorne à SuperPets, acesse seu painel administrativo e siga os seguintes passos:</p>
+      <p>1 - Complete seu cadastro adicionando sua foto e contando um pouco de você;</p>
+      <p>2 - Cadastre seus serviços e defina o preço;</p>
+      <p>3 - Destaque as regiões de atendimento, ele será importante para que os clientes possam te encontrar;</p>
+      <p>4 - Mantenha sempre sua agenda atualizada;</p>
+      <p>Qualquer dúvida, nos contate por um dos nossos meios de contato.</p>
+      <p>Desejamos a você ótimos negócios e uma maravilhosa jornada!</p>
+      <br>
+      <p>Equipe SuperPets</p>
+    `;
+
+    text = `Seja bem vindo(a) ${professional.name} a comunidade SuperPets Profissional!\n\nÉ um prazer tê-lo conosco.\nPara iniciar sua jornada, retorne à SuperPets, acesse seu painel administrativo e siga os seguintes passos:\n\n1 - Complete seu cadastro adicionando sua foto e contando um pouco de você;\n\n2 - Cadastre seus serviços e defina o preço;\n3 - Destaque as regiões de atendimento, ele será importante para que os clientes possam te encontrar;
+    \n4 - Mantenha sempre sua agenda atualizada;\n\nQualquer dúvida, nos contate por um dos nossos meios de contato.\nDesejamos a você ótimos negócios e uma maravilhosa jornada!\n\nEquipe SuperPets`;
+
+    subject = `Olá ${professional.name}, seja bem vindo(a) a comunidade SuperPets Profissional!`;
+  } 
+
+  try {
+    let msg = {
+      to: `${professional.email}`,
+      from: 'superpets.sp@gmail.com',
+      subject,
+      text,
+      html
+    }
+    await sgMail.send(msg);
+  } catch(error) {
+    console.error(error);
+    if (error.response) {
+      console.error(error.response.body)
+    }
+  }
 }
 
 module.exports = controller;
